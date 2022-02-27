3d26cb8d281a082bd13ded0bb43da780cc9cb89c
FilipSpirovski <spirovskii.filipp@gmail.com>
2022-02-27 18:29:15 +0100

    initial commit

---
+++ app/src/main/java/mk/ukim/finki/mpip/housing_service/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package mk.ukim.finki.mpip.housing_service.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/mk/ukim/finki/mpip/housing_service/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package mk.ukim.finki.mpip.housing_service.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
